7b6de60aec2b2e0c6cdb0d1cb361949fac1342d1
san4e <san4es.kom@gmail.com>
2024-05-21 17:13:09 +0300

    feat: Added some custom styles and properties. Icons and images imported. Aliases added.

---
+++ src/assets/images/RightArrow.jsx
@@ -0,0 +1,15 @@
+const RightArrow = () => {
+    return (
+        <svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.25 1.125L5.625 5.5L1.25 9.875"
+                  stroke="#3366FF"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+            />
+        </svg>
+
+    );
+};
+
+export default RightArrow;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", 'sans-serif'],
+        mulish: ["Mulish", 'sans-serif']
+      },
+      colors: {
+        primary: "#4B4B4B",
+        secondary: "#252323",
+        bg: "#EEF0F2",
+        border: "#C0C0C0",
+        placeholder: "#3B3B3B",
+        cta: "#3366FF",
+
+        card1: "#FFC800",
+        card2: "#2E68C0",
+        card3: "#EA1542",
+        card4: "#1D4ED8"
+      }
+    },
   },
   plugins: [],
 }
