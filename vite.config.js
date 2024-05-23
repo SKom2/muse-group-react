dc19acf558bc4147d0c98b3b3cfb3e273ba8e84d
san4e <san4es.kom@gmail.com>
2024-05-23 14:30:37 +0300

    feat: base url for gh-pages added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/muse-group-react',
   plugins: [react()],
   resolve: {
     alias: {
