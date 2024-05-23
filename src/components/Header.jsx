2d2fd1fe67fc4256facae62a0272bf6276a3f1f6
san4e <san4es.kom@gmail.com>
2024-05-23 18:01:03 +0300

    feat: changed the icon in tab and added link to the Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import Logo from "@images/Logo.png"
 const Header = () => {
     return (
         <header>
-            <div className="py-6 sm:py-8">
-                <img src={Logo} alt="Muse group logo" className="w-[144px] h-10 m-auto"/>
+            <div className="py-6 sm:py-8 text-center">
+                <a href="https://www.mu.se/" className="inline-block">
+                    <img src={Logo} alt="Muse group logo" className="w-[144px] h-10"/>
+                </a>
             </div>
         </header>
     );
